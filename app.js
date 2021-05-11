21eb9533669dd1d665d74ff12a1701f00bcbe666
nkalia1802 <n.kalia@student.tudelft.nl>
2021-05-11 23:27:16 +0200

    Fix request parsing

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
 // use the express-static middleware
-app.use(express.static("public"))
+app.use(express.static("public"));
+app.use(express.json());
+// app.use(express.urlencoded());
 
 // define the first route
 app.get("/", function (req, res) {
   res.send("Hello World!")
-})
+});
+
+app.post("/post", function (req, res) {
+    console.log(req.body);
+    res.send(req.body)
+});
 
 // start the server listening for requests
 app.listen(process.env.PORT || 3000,
