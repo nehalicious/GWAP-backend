ee4567a708a9fcc5573b3d35024451e513c6cacc
nkalia1802 <n.kalia@student.tudelft.nl>
2021-05-13 23:54:06 +0200

    Basic socket

--- app.js
+++ app.js
@@ -1,35 +1,25 @@
-const express = require("express");
+const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
+
 const app = express();
-const mongoose = require('mongoose');
-
-const room = require('./game/room');
-const player = require('./game/player');
-const cors = require('cors');
-
-// use the express middleware
-app.use(express.static("public"));
-app.use(express.json());
-app.use(cors({
-  origin: 'http://localhost:3000'
-}));
-
-
-const url = "mongodb+srv://nkalia:Buddy2008@cluster0.f61r4.mongodb.net/game?retryWrites=true&w=majority"
-mongoose.connect(url, { useNewUrlParser: true });
-
-const db = mongoose.connection;
-
-db.once('open', _ => {
-    console.log('Database connected:', url)
+const server = http.createServer(app);
+const io = socketio(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }
 });
 
-db.on('error', err => {
-    console.error('connection error:', err)
+io.on('connection', socket => {
+    console.log("new websocket connection");
+    socket.emit('message', 'welcome')
+
+    // socket.on('player', ({name}) => {
+    //     console.log("make player")
+    // })
 });
 
-app.listen(process.env.PORT || 8000,
-    () => console.log("Server is running..."));
+const PORT = 8000 || process.env.port;
 
-app.post('/createRoom', room.createRoom);
-app.post('/addToRoom', room.addToRoom);
-app.post('/player', player.createPlayer);
+server.listen(PORT, ()=> console.log(`Server is running on port ${PORT}`))
