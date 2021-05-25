1d15800cb88f05e89264cb46b84a1c6cb101d535
nkalia1802 <n.kalia@student.tudelft.nl>
2021-05-25 22:27:18 +0200

    update server

--- app.js
+++ app.js
@@ -26,8 +26,8 @@ db.on('error', err => {
 
 const io = socketio(server, {
     cors: {
-        origin: "http://localhost:3000",
-        // origin: 'https://gwap-frontend.vercel.app',
+        // origin: "http://localhost:3000",
+        origin: 'https://gwap-frontend.vercel.app',
         methods: ["GET", "POST"]
     }
 });
