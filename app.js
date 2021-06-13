13d6f51585540294509501415f021b6897660db3
nkalia1802 <n.kalia@student.tudelft.nl>
2021-06-13 16:14:49 +0200

    reset server

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 
 const server = http.createServer(app);
 
-const url = "mongodb+srv://nkalia:Buddy2008@cluster0.f61r4.mongodb.net/trial?retryWrites=true&w=majority"
+const url = "mongodb+srv://nkalia:Buddy2008@cluster0.f61r4.mongodb.net/game?retryWrites=true&w=majority"
 mongoose.connect(url, { useNewUrlParser: true });
 
 const db = mongoose.connection;
@@ -28,8 +28,8 @@ db.on('error', err => {
 
 const io = socketio(server, {
     cors: {
-        origin: "http://localhost:3000",
-        // origin: 'https://gwap-frontend.vercel.app',
+        // origin: "http://localhost:3000",
+        origin: 'https://gwap-frontend.vercel.app',
         methods: ["GET", "POST"]
     }
 });
